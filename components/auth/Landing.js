1dd142a6470d75c4d206d1c4bbc60d01cb5620b8
Abrahamcepe <abrahamgolf@gmail.com>
2021-01-01 15:13:04 -0600

    Added landing and register components

---
+++ components/auth/Landing.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { View, Text, StyleSheet, Button } from 'react-native'
+
+export default function Landing({ navigation }) {
+    return (
+        <View style={styles.container}>
+            <Button title="Register" onPress={() => navigation.navigate('Register')}/>
+            <Button title="Login" onPress={() => navigation.navigate('Login')}/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
