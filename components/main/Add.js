2d9d93eacd36028b02a0afc317782ff0f0e107fd
Abrahamcepe <abrahamgolf@gmail.com>
2021-01-14 11:55:43 -0600

    setup image picker

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,17 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import { Camera } from 'expo-camera';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function Add() {
-  const [hasPermission, setHasPermission] = useState(null);
+  const [cameraPermission, setCameraPermission] = useState(null);
+  const [galleryPermission, setGalleryPermission] = useState(null);
   const [camera, setCamera] = useState(null);
   const [image, setImage] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
 
   useEffect(() => {
     (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
+      const cameraStatus = await Camera.requestPermissionsAsync();
+      setCameraPermission(cameraStatus.status === 'granted');
+
+      if (Platform.OS !== 'web') {
+        const galleryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        setGalleryPermission(galleryStatus.status === 'granted')
+      }
+
     })();
   }, []);
 
@@ -22,10 +30,25 @@ export default function Add() {
     }
   }
 
-  if (hasPermission === null) {
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+  if (cameraPermission === null || galleryPermission === null) {
     return <View />;
   }
-  if (hasPermission === false) {
+  if (cameraPermission === false || galleryPermission === false) {
     return <Text>No access to camera</Text>;
   }
   return (
@@ -48,6 +71,12 @@ export default function Add() {
             title="Take Picture"
             onPress={() => {
                 takePicture()
+            }}
+        />
+        <Button
+            title="Pick Image"
+            onPress={() => {
+                pickImage()
             }}
         />
         {image && <Image source={{uri: image}} style={{flex: 1}}/>}
