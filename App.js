2f66ee9deff972814a9480f2090d45270bc2bc13
Abrahamcepe <abrahamgolf@gmail.com>
2021-01-14 11:18:07 -0600

    Setup tab navigation

--- App.js
+++ App.js
@@ -16,6 +16,8 @@ import LandingScreen from './components/auth/Landing';
 import RegisterScreen from './components/auth/Register';
 import LoginScreen from './components/auth/Login';
 import MainScreen from './components/Main';
+import AddScreen from './components/main/Add';
+
 const Stack = createStackNavigator();
 
 
@@ -66,7 +68,12 @@ export class App extends Component {
     }
     return(
       <Provider store={store}>
-        <MainScreen/>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Main">
+            <Stack.Screen name="Main" component={MainScreen} options={{headerShown: false}}/>
+            <Stack.Screen name="Add" component={AddScreen}/>
+          </Stack.Navigator>
+        </NavigationContainer>
       </Provider>
     )
     
